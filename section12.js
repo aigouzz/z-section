96860de39b19add25b7ae185bf104239a9b7b14a
gxc <1003823149@qq.com>
2022-10-26 14:39:01 +0800

    feat:add algorithm

--- section12.js
+++ section12.js
@@ -31,6 +31,17 @@
 
    响应拦截器： 这块就是根据 后端`返回来的状态码判定执行不同业务
  */
+/**
+ * 了解业务：全面调研当前业务和竞品的现状，充分理解当前渲染链路和节点，确认当前存在的问题
+寻找方案：预估未来发展的方向，尽可能多的了解相关解决方案或创新自己的方案，比如：SSR，ER，预渲染，预加载，静态化等
+评估方案：和相关同学讨论或开会，评估所有可行的方案及其合适度、复杂度、前瞻性和 ROI。选出至少一个候选方案，比如：SSR
+Demo 开发：基于现有开发能力为所有候选方案开发对应 Demo，提前探路并验证风险和可行性，帮助产出更合适的方案设计
+方案设计：梳理清楚 SSR 完整链路上相关节点和合作方，多写、多画、多思考、多讨论相关架构和设计，深入细节产出 RFC 文档
+RFC 评审：充分评审设计、实现和产物细节，可多次评审直至所有成员达成共识。确定相关开发和团队分工，保证方案完善可执行
+落到实处：推进项目开发，多与开发团队沟通，并至少参与一部分编码工作，打通所有相关开发和运维链路，保障产物简单好用
+沉淀传承：沉淀文档，通过会议、分享或文章帮助其他人理解 SSR 方案和架构，用好 SSR。做好答疑，并推动方案实施
+不断演进：关注 SSR 的发展，演进已有链路，比如，个性化的 SSR，结合 ER 的 SSR 等
+ */
 //实现简易版本axios
 class Axios{
    constructor() {
@@ -242,4 +253,75 @@ function diffNumbers(num, trg){
 console.log(diffNumbers(numbers, target))
 //[[1, 2], [3, 6]]]
 
+}
+{
+  function findIP(onNewIP) {
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({iceServers: [{urls: "stun:stun.l.google.com:19302"}]}),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
+    
+    function ipIterate(ip) {
+      if (!localIPs[ip]) onNewIP(ip);
+      localIPs[ip] = true;
+    }
+      
+    pc.createDataChannel("");
+     
+    pc.createOffer(function(sdp) {
+      sdp.sdp.split('\n').forEach(function(line) {
+        if (line.indexOf('candidate') < 0) return;
+        line.match(ipRegex).forEach(ipIterate);
+      });
+      pc.setLocalDescription(sdp, noop, noop);
+    }, noop);
+      
+    pc.onicecandidate = function(ice) {
+      if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+      ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+    };
+    }
+    
+    function addIP(ip) {
+    console.log('got ip: ', ip);
+    }
+    // findIP(addIP);
+}
+{
+  function findMax(str) {
+    let set = new Set();
+    let rk = -1;
+    let str1 = '';
+    let res = 0;
+    let length = str.length;
+    for(let i = 0;i < length;i ++) {
+      if(i !== 0) {
+        set.delete(str.charAt(i - 1));
+      }
+      while(rk + 1 < i && !set.has(str.charAt(rk + 1))) {
+        set.add(str.charAt(rk + 1));
+        rk ++;
+      }
+      if(res < rk + 1 - i) {
+        str1 = str.substring(res, rk + 1 - i);
+        res = rk + 1 - i;
+      }
+    }
+    return {str1, res};
+  }
+  function findMax1(arr) {
+    let sum = 0;
+    let res = arr[0];
+    for(let i = 0;i < arr.length;i ++) {
+      if(sum > 0) {
+        sum += arr[i];
+      } else {
+        sum = arr[i];
+      }
+      res = Math.max(res, sum);
+    }
+    return res;
+  }
 }
